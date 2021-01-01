087a91e5adaf7091fa649e3fb6abe6c9c0c97b14
billykore <billyimmcul2010@gmail.com>
2021-01-01 20:41:11 +0800

    Commit pertama web Sungkem

---
+++ src/components/Maintenance.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button'
+import underConstructionJpg from '../assets/img/under_construction.svg';
+
+export default function Maintenance() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Under <br /> Construction</h1>
+          <p>We need some time too build our website.</p>
+          <Button variant="warning" size="lg">Contact Us</Button>
+        </Col>
+        <Col>
+          <img src={underConstructionJpg} alt="Under Construction" />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Nav() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand>SUNGKEM</Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
+}
