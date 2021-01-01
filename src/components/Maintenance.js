9b8b25394fb02c18373c04863bcfd6086a640b4e
billykore <billyimmcul2010@gmail.com>
2021-01-01 22:52:31 +0800

    Add Whatsapp number

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -6,13 +6,15 @@ import Button from 'react-bootstrap/Button'
 import underConstructionJpg from '../assets/img/under_construction.svg';
 
 export default function Maintenance() {
+  const whatsapp = 'https://wa.me/+6285856083156';
+
   return (
     <Container>
       <Row>
         <Col>
           <h1>Under <br /> Construction</h1>
           <p>We need some time too build our website.</p>
-          <Button variant="warning" size="lg">Contact Us</Button>
+          <Button href={whatsapp} variant="warning" size="lg">Contact Us</Button>
         </Col>
         <Col>
           <img src={underConstructionJpg} alt="Under Construction" />
