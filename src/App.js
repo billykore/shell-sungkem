29a4d2ad8fcae17fdb00749550e23b3a3367a299
billykore <billyimmcul2010@gmail.com>
2021-01-08 19:18:36 +0800

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Navbar } from './components';
 import GlobalStyle from './globalStyle';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
@@ -9,5 +9,5 @@ export default function App() {
       <GlobalStyle />
       <Navbar />
     </Router>
-  );
+  )
 }
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,54 +1,51 @@
-import styled, { createGlobalStyle } from 'styled-components';
-
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
     margin: 0;
     padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
   }
-
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
   body {
     font-family: 'Roboto', sans-serif;
-    font-weight: 400;
-  }
-`;
-
-export const Container = styled.div`
-  z-index: 1;
-  width: 100%;
-  max-width: 1300px;
-  margin-right: auto;
-  margin-left: auto;
-  padding-right: 50px;
-  padding-left: 50px;
-
-  @media screen and (max-width: 991px) {
-    padding-right: 30px;
-    padding-left: 30px;
-  }
-`;
-
-export const Button = styled.button`
-  border-radius: 4px;
-  background: ${({ primary }) => (primary ? '#FFC107' : '#0467FB')};
-  white-space: nowrap;
-  padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
-  color: #1C2034;
-  font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
-  font-weight: 500;
-  outline: none;
-  border: none;
-  cursor: pointer;
-
-  &:hover {
-    transition: all 0.3s ease-out;
-    background: #fff;
-    background-color: ${({ primary }) => (primary ? '#E8AE00' : '#4B59F7')};
-  }
-
-  @media screen and (max-width: 960px) {
-    width: 100%;
   }
 `;
 
